4fec4caf3ed31b6dcb8698d0514c8a65b920be7a
Odralix <ossian.edstrom@live.se>
2018-04-06 11:26:18 +0200

    Added folders and test to main
    Added Oscripts and Omaterial folders to Main and made a small test area in positive Z-direction from origin in the main level. This includes collisions/death scripts. THE SMOOTH FOLLOW SCRIPT HAS BEEN ADDED.

---
+++ GitTest/Assets/Code/OScripts/bMoveScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bMoveScript : MonoBehaviour
+{
+
+    public float moveSpeed;
+    //public Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        moveSpeed = 10f;
+	}
+
+    // Update is called once per frame
+    void Update ()
+    {
+        //rb.AddForce(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        transform.Translate(moveSpeed*Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")* Time.deltaTime);
+    }
+}
---
+++ GitTest/Assets/Code/OScripts/sCollisionScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sCollisionScript : MonoBehaviour
+{
+    public float hitCount = 0;
+    public Material newMaterialref;
+    public Material sMaterialref;
+    public Renderer refRend;
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);
+
+        if(collisionInfo.collider.tag == "Obstacle")
+        {
+            hitCount++;
+
+            if(hitCount == 1 )
+            {
+                refRend.material = newMaterialref;
+            }
+            else if (hitCount == 2)
+            {
+                refRend.material = sMaterialref;
+            }
+            else if (hitCount == 3)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+}
