16386e4904fdb46c27edd51a5aacc6f0f0b4ad5d
SocioBlade <timpalainen@gmail.com>
2018-04-06 12:00:04 +0200

    Simple movement of player.

--- GitTest/Assets/Code/Scripts/t_PlayerMovement.cs
+++ GitTest/Assets/Code/Scripts/t_PlayerMovement.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class t_PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Rigidbody rb;
+
+    public float playerMovement = 700f;
+    public float forwardForce = 2000f;
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        // Add a forward force
+        if (Input.GetKey("w")) {
+            rb.AddForce(0, 0, playerMovement * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s")) {
+            rb.AddForce(0, 0, (-playerMovement / 2) * Time.deltaTime);
+        }
+
+        if (Input.GetKey("d")) {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a")) {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        }
+
 	}
 }
