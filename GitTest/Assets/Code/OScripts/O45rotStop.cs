cf7d23e4bd9fe0b56784af665bc6a2e73d779593
Odralix <ossian.edstrom@live.se>
2018-04-06 13:25:15 +0200

    Worked on O45rotStop
    Meant to stop an object from rotating more than 45 degrees in z and x axis.

---
+++ GitTest/Assets/Code/OScripts/O45rotStop.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class O45rotStop : MonoBehaviour {
+
+    //public Rigidbody pRb;
+    //public Vector3 rotVec;
+    // Use this for initialization
+    public Transform tLook;
+
+	void Start ()
+    {
+        //pRb = GetComponent<Rigidbody>();
+        //rotVec = new Vector3(pRb.rotation.x, pRb.rotation.y, -0.3f);
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        print(gameObject.transform.rotation.eulerAngles.z);
+        if (gameObject.transform.rotation.eulerAngles.z >= 45)
+        {
+            transform.Rotate(gameObject.transform.rotation.eulerAngles.x, gameObject.transform.rotation.eulerAngles.y, 45);
+        //    print("WHA!?");
+        //    transform.Rotate(transform.rotation.x, transform.rotation.y, 45);
+        }
+        else if (gameObject.transform.rotation.eulerAngles.z <= -90)
+        {
+            transform.Rotate(gameObject.transform.rotation.eulerAngles.x, gameObject.transform.rotation.eulerAngles.y, 0f);
+            //    print("WHA!?");
+            //    transform.Rotate(transform.rotation.x, transform.rotation.y, 45);
+        }
+        //else if (tLook.rotation.z <= -45)
+        //{
+        //    print("NEGATIVE WHA!?");
+        //    transform.Rotate(transform.rotation.x, transform.rotation.y, -45);
+        //}
+
+        //if ((GetComponent<Rigidbody>().rotation.z <= -0.3))
+        //{
+        //    rotVec = new Vector3(pRb.rotation.x, pRb.rotation.y, -0.3f);
+        //    Quaternion deltaRotation = Quaternion.Euler(rotVec * Time.deltaTime);
+        //    pRb.MoveRotation(pRb.rotation * deltaRotation);
+        //}
+        //print(GetComponent<Rigidbody>().rotation.z);
+    }
+}
