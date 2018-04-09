4a3d6bd14392b063caa8dfe2182089ff616d12aa
Odralix <ossian.edstrom@live.se>
2018-04-09 12:40:20 +0200

    Minor changes and added O_Thruster and O_Hover
    New attempts at hover movement

--- GitTest/Assets/Code/OScripts/O_ThrusterScript.cs
+++ GitTest/Assets/Code/OScripts/O_ThrusterScript.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class O_ThrusterScript : MonoBehaviour {
 
     public float thrusterStength;
-    public float distance;
+    public float thrusterDistance;
     public Transform[] thrusters;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate ()
+    {
+        RaycastHit hit;
+        
+        foreach(Transform thruster in thrusters)
+        {
+            Vector3 downwardForce;
+            float distancePercentage;
+
+            if(Physics.Raycast (thruster.position, thruster.up *-1, out hit, thrusterDistance))
+            {
+                //The thruster is within thrusterdistance to the ground. How far away?
+                distancePercentage = 1 - (hit.distance / thrusterDistance);
+
+                //calculate how hard to push
+                downwardForce = transform.up * thrusterStength * distancePercentage;
+                // Correct it for the mass of the mesh and deltatime
+                downwardForce = downwardForce * Time.deltaTime * GetComponent<Rigidbody>().mass;
+
+                //apply at thruster location
+                GetComponent<Rigidbody>().AddForceAtPosition(downwardForce, thruster.position);
+            }
+        }
 	}
 }
