8e32167691ff45b9979c35da6df96daf9c3318dd
Odralix <ossian.edstrom@live.se>
2018-04-09 09:29:33 +0200

    Finished O_PowerUp script
    Added a duration and made sure the particleSystem is removed properly.

--- GitTest/Assets/Code/OScripts/O_powerUp.cs
+++ GitTest/Assets/Code/OScripts/O_powerUp.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class O_powerUp : MonoBehaviour
 {
     public GameObject pickupEffect;
+    public float duration = 4f;
+    private GameObject clone;
 
     public float multiplier = 3f;
 
@@ -12,21 +14,44 @@ public class O_powerUp : MonoBehaviour
     {
         if(other.CompareTag("Player"))
         {
-            Pickup(other);
+            StartCoroutine( Pickup(other) );
         }
     }
 
-    void Pickup(Collider player)
+    IEnumerator Pickup(Collider player)
     {
         //Spawn Cool Effect
-        Instantiate(pickupEffect, transform.position, transform.rotation);
+        clone = Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        //Waiting allows for the powerUp object to still be visible. This removes it.
+
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+
+        //pickupEffect.transform.SetParent(gameObject.transform);
 
         //Apply effect to the player
         bMoveScript speed = player.GetComponent<bMoveScript>();
 
         speed.moveSpeed *= multiplier;
 
+        //wait x amount of seconds
+
+        yield return new WaitForSeconds(1f);
+
+        //Stops the particleSystem from repeating itself during the duration. 
+        //Must be placed after small delay as putting it higher destroys the system before it is seen.
+        clone.GetComponent<ParticleSystem>().Stop();
+
+        yield return new WaitForSeconds(duration-1);
+
+
+        //reverse effect on our player
+
+        speed.moveSpeed /= multiplier;
+
         //Remove power up object
+        Destroy(clone);
         Destroy(gameObject);
 
     }
